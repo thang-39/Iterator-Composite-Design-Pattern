f39a2aa13e68a3077b992938c4eda446a20c6e1e
thang-39 <trantruongminhthang@gmail.com>
2023-03-23 14:30:44 +0700

    1st commit - Iterator Pattern

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        PancakeHouseMenu pancakeHouseMenu = new PancakeHouseMenu();
+//        ArrayList breakfastItems = pancakeHouseMenu.getMenuItems();
+        DinerMenu dinerMenu = new DinerMenu();
+//        MenuItem[] lunchItems = dinerMenu.getMenuItems();
+
+//        for (Object breakfastItem : breakfastItems) {
+//            MenuItem menuItem = (MenuItem) breakfastItem;
+//            System.out.print(menuItem.getName() + " ");
+//            System.out.println(menuItem.getPrice() + " ");
+//            System.out.println(menuItem.getDescription());
+//        }
+//        for (MenuItem menuItem : lunchItems) {
+//            System.out.print(menuItem.getName() + " ");
+//            System.out.println(menuItem.getPrice() + " ");
+//            System.out.println(menuItem.getDescription());
+        }
+
+
+    }
